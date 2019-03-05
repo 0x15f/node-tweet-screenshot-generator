34125f85b14d24da346b8b177623e54f5bf190d1
Jake Casto <jake@snaju.com>
2019-03-05 17:21:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,41 +3,48 @@ const app = express()
 const port = 3001
 
 app.get('/', async (req, res) => {
-	const puppeteer = require('puppeteer')
+    try {
+        const puppeteer = require('puppeteer')
 
-	const browser = await puppeteer.launch({
-		headless: true,
-		args: ['--unlimited-storage', '--disable-dev-shm-usage', '--no-sandbox', '--disable-setuid-sandbox']
-	});
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--unlimited-storage', '--disable-dev-shm-usage', '--no-sandbox', '--disable-setuid-sandbox']
+        });
 
-	const page = await browser.newPage();
+        const page = await browser.newPage();
 
-	await page.goto(decodeURIComponent(req.query.url), {waitUntil: ['load'], timeout: 30000});
+        await page.goto(decodeURIComponent(req.query.url), {waitUntil: ['load'], timeout: 30000});
 
-    const selector = '.tweet[data-tweet-id="' + req.query.id + '"]';
+        const selector = '.tweet[data-tweet-id="' + req.query.id + '"]';
 
-    const rect = await page.evaluate(selector => {
-        const element = document.querySelector(selector);
-        if (!element)
-            return null;
-        const {x, y, width, height} = element.getBoundingClientRect();
-        return {left: x, top: y, width, height, id: element.id};
-    }, selector);
+        const rect = await page.evaluate(selector => {
+            const element = document.querySelector(selector);
+            if (!element)
+                return null;
+            const {x, y, width, height} = element.getBoundingClientRect();
+            return {left: x, top: y, width, height, id: element.id};
+        }, selector);
 
-    if (!rect)
-        throw Error(`Could not find element that matches selector: ${selector}.`);
+        if (!rect)
+            throw Error(`Could not find element that matches selector: ${selector}.`);
 
-    buffer = await page.screenshot({
-        clip: {
-            x: rect.left,
-            y: rect.top,
-            width: rect.width,
-            height: rect.height
-        }
-    });
+        buffer = await page.screenshot({
+            clip: {
+                x: rect.left,
+                y: rect.top,
+                width: rect.width,
+                height: rect.height
+            }
+        });
 
-    res.type('image/png');
-    res.send(buffer);
+        res.type('image/png');
+        res.send(buffer);
+    }
+    catch(er)
+    {
+        console.log(er);
+        res.send('error');
+    }
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
