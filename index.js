dbfc44c34730fb8d7b28dc748a1ac95882f1a4b0
Jake Casto <jake@snaju.com>
2019-03-05 19:45:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ app.get('/', async (req, res) => {
 
         await page.goto(decodeURIComponent(req.query.url), {waitUntil: ['load'], timeout: 30000});
 
-        // page.setViewport({
-        //     width: 3600,
-        //     height: 2400
-        // });        
+        page.setViewport({
+            width: 3600,
+            height: 2400
+        });        
 
         const selector = '.tweet[data-tweet-id="' + req.query.id + '"]';
 
@@ -27,8 +27,8 @@ app.get('/', async (req, res) => {
             if (!element)
                 return null;
 
-            element.style.width = 3600;
-            element.style.height = 2400;
+            element.style.width = '1350px';
+            element.style.height = '525px';
             const {x, y, width, height} = element.getBoundingClientRect();
             return {left: x, top: y, width, height, id: element.id};
         }, selector);
