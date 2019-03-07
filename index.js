a37ca072d6c39a67ef33b94cf2b59b3701bf72ea
Jake Casto <jake@snaju.com>
2019-03-07 13:52:48 -0500

    Add padding option

--- index.js
+++ index.js
@@ -21,17 +21,28 @@ app.get('/', async (req, res) => {
         });        
 
         const selector = '.tweet[data-tweet-id="' + req.query.id + '"]';
+        const padding = typeof req.query.padding !== 'undefined';
 
-        const rect = await page.evaluate(selector => {
+        const rect = await page.evaluate((selector, padding) => {
             const element = document.querySelector(selector);
             if (!element)
                 return null;
 
-            element.style.width = '1350px';
-            element.style.height = '525px';
+            if(padding === true)
+            {
+                element.style.width = '1350px';
+                element.style.height = '621px';
+                element.querySelector('.content:first-child, .clearfix:first-child').style.marginTop = '96px';
+            }
+            else
+            {
+                element.style.width = '1350px';
+                element.style.height = '525px'
+            }
+
             const {x, y, width, height} = element.getBoundingClientRect();
             return {left: x, top: y, width, height, id: element.id};
-        }, selector);
+        }, selector, padding);
 
         if (!rect)
             throw Error(`Could not find element that matches selector: ${selector}.`);
